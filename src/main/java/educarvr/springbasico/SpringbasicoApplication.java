9ae291fea49a91414c571d5f9c979508a222c975
JUAN CARLOS CAMPOS <rapidclimate@gmail.com>
2023-04-18 18:42:49 -0600

    monetin

--- src/main/java/educarvr/springbasico/SpringbasicoApplication.java
+++ src/main/java/educarvr/springbasico/SpringbasicoApplication.java
@@ -1,11 +1,14 @@
 package educarvr.springbasico;
 
+import educarvr.springbasico.config.TiendaConfig;
 import educarvr.springbasico.dominio.Libro;
 import educarvr.springbasico.dominio.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringbasicoApplication implements CommandLineRunner {
@@ -19,6 +22,13 @@ Libro libroA;
 
 	@Override
 	public void run(String... args) throws Exception {
+/*
+Normalmente se accede a los beans por medio de la clase
+AnnotatioConfigApplicationContext
+ */
+	ApplicationContext ctx= new AnnotationConfigApplicationContext(TiendaConfig.class);
+	Libro libro=  ctx.getBean("libroA", Libro.class);
 		System.out.println(libroA.getTitulo());
+		System.out.println(libro.getTitulo());
 	}
 }
